21f1ff71c458ea8924bf20e2a6aa2274fd866371
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-08 20:55:23 -0300

    Delete src/main/java/com/linuxtips/descomplicandojavaespring/day14 directory

--- src/main/java/com/linuxtips/descomplicandojavaespring/day14/methodReference/Method.java
+++
@@ -1,8 +0,0 @@
-package com.linuxtips.descomplicandojavaespring.day14.methodReference;
-
-public class Method {
-
-    public static  void programarEmJava(){
-        System.out.println("Eu programo em Java");
-    }
-}
