afdbf53760a6dbf823bf7da96a73189bdf0169f8
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-08 14:50:14 -0300

    meu arquivo Repeticao.java

--- src/main/java/com/linuxtips/descomplicandojavaespring/day04/Repeticao.java
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day04/Repeticao.java
@@ -4,14 +4,18 @@ import java.util.Scanner;
 
 public class Repeticao {
 
-    //for
+   
     public static void main(String[] args) {
+         //for 
+         //numero de iteracoes limitado
         for (int i = 0; i <= 100; i++) {
             System.out.println("Nessa repeticao i está valendo  " + i);
         }
 
 
         //while
+        //numero de iteracoes ilimitado
+        // pode  executar nenhuma vez a acao
         Scanner in = new Scanner(System.in);
         var valorSecreto = 0;
         while (valorSecreto != 12345) {
@@ -25,7 +29,8 @@ public class Repeticao {
         }
 
 
-        //do-while
+        //do-while (faça enquanto tal coisa nao acontece)
+        // ele executa ao mesmo uma vez a iteracao
         int valor = 10;
         do {
             System.out.println(valor);
