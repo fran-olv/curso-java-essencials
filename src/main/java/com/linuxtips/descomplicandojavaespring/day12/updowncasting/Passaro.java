4c87de69d4abfeae45669ba1084d4b8a4e7da949
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-08 20:54:52 -0300

    Delete src/main/java/com/linuxtips/descomplicandojavaespring/day12 directory

--- src/main/java/com/linuxtips/descomplicandojavaespring/day12/updowncasting/Passaro.java
+++
@@ -1,8 +0,0 @@
-package com.linuxtips.descomplicandojavaespring.day12.updowncasting;
-
-public class Passaro extends  Animal{
-    @Override
-    public void comer() {
-        super.comer();
-    }
-}
