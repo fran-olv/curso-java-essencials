3b8b3cc2f6429d8cff347e219d9890f8faec8293
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-08 21:45:55 -0300

    minha Conta.java

--- src/main/java/com/linuxtips/descomplicandojavaespring/day06/abstracao/interfaces/Conta.java
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day06/abstracao/interfaces/Conta.java
@@ -1,6 +1,11 @@
 package com.linuxtips.descomplicandojavaespring.day06.abstracao.interfaces;
 
 interface  Conta {
+    //implementacao via contrato
+    //modificadores de acesso: é tudo publico
+    //bom pra implementar herança e polimorfismo
+    //interfaces + utilizadas que classes abstratas
+    
     public void consultarSaldo();
     public  void fazerPix();
 }
