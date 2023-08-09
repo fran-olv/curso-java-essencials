dc75f5044d846a9912901bf998884b43002a0a05
Franciele B. de Oliveira <32960110+fran-olv@users.noreply.github.com>
2023-08-08 22:33:57 -0300

    Update Botao.java

--- src/main/java/com/linuxtips/descomplicandojavaespring/day07/polimorfismo/sobrescrita/Botao.java
+++ src/main/java/com/linuxtips/descomplicandojavaespring/day07/polimorfismo/sobrescrita/Botao.java
@@ -1,7 +1,7 @@
 package com.linuxtips.descomplicandojavaespring.day07.polimorfismo.sobrescrita;
 
 public class Botao {
-
+//sobrecrita muda apenas a implementacao
 
     public void ligar() {
         System.out.println("ligando");
